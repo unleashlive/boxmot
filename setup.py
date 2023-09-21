8ff324a463b1f38b05d34131a17d0f64de86697d
iXez <1274945+ixez@users.noreply.github.com>
2023-09-21 21:44:57 +1000

    Updated setup.py for cloud deployment

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
-from pathlib import Path
-import pkg_resources as pkg
 from setuptools import find_packages, setup
-
-# Settings
-FILE = Path(__file__).resolve()
-PARENT = FILE.parent  # root directory
-README = (PARENT / 'README.md').read_text(encoding='utf-8')
-REQUIREMENTS = [f'{x.name}{x.specifier}' for x in pkg.parse_requirements((PARENT / 'requirements.txt').read_text())]
-
 
 setup(
     name='boxmot',  # name of pypi package
@@ -15,7 +6,6 @@ setup(
     python_requires='>=3.8',
     license='AGPL-3.0',
     description=('SOTA tracking methods for detection, segmentation and pose estimation models.'),
-    long_description=README,
     long_description_content_type='text/markdown',
     url='https://github.com/mikel-brostrom/yolov8_tracking',
     project_urls={
@@ -25,6 +15,6 @@ setup(
     author_email='yolov5.deepsort.pytorch@gmail.com',
     packages=find_packages(),  # required
     include_package_data=True,
-    install_requires=REQUIREMENTS,
+    install_requires=[],
     platforms=["linux"]
 )
